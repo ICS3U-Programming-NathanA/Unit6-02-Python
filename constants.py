2b85b2df9e22db975839fb99910af47ad6a87e0f
Nathan Araujo <nathan.araujo@stu.ocsb.ca>
2022-12-20 19:08:44 +0000

    This program generates 10 random numbers

---
+++ constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by: Chris Di Bert
+# Date: Dec.9, 2022
+# This file contains the necessary constants
+# needed for average_calc.py
+
+MAX_ARRAY_SIZE = 10
+MIN = 0
+MAX = 100
---
+++ max_value.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# Created by: Nathan Araujo
+# Date: Dec.15, 2022
+# This program generates 10 random numbers and
+# prints the largest  out of the list of 10 random numbers
+
+import random
+import constants
+
+
+def max_value(num_list):
+
+    highest_num = -1
+
+    # Use a for loop to see which number is the largest number in the list
+    for counter in range(constants.MAX_ARRAY_SIZE):
+        if num_list[counter] > highest_num:
+            highest_num = num_list[counter]
+
+    # return highest_num
+    return highest_num
+
+
+def main():
+    # Creates a empty list called random_num
+    random_num = []
+
+    # For loop that generates 10 random number between 0 and 100
+    for counter in range(constants.MAX_ARRAY_SIZE):
+
+        # generates a number between 0 and 100
+        random_int = random.randint(constants.MIN, constants.MAX)
+
+        # Adds number to the list
+        random_num.append(random_int)
+
+        # print that it added the number to the index
+        print(f"Added {random_int} to list at index {counter}")
+
+    # Stores largest number generated in variable
+    large_value = max_value(random_num)
+
+    # Prints the largest number generated
+    print(f"The max value is: {large_value}.")
+
+
+if __name__ == "__main__":
+    main()
